ea818656e5ebf686d0d30fa165a61c153a82f8a7
lucidrains <lucidrains@gmail.com>
2024-02-15 06:37:36 -0800

    scaffold

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'ring-attention-pytorch',
+  packages = find_packages(exclude=[]),
+  version = '0.0.1',
+  license='MIT',
+  description = 'Ring Attention - Pytorch',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/lucidrains/ring-attention-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'distributed attention'
+  ],
+  install_requires=[
+    'einops>=0.7.0',
+    'torch>=2.0'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
