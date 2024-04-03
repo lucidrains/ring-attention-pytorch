73e58dc786168644b40de3bae1f0c37fc2e4829c
lucidrains <lucidrains@gmail.com>
2024-04-03 09:09:24 -0700

    include einops for now for ring cuda kernel version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'ring-attention-pytorch',
   packages = find_packages(exclude=[]),
-  version = '0.2.16',
+  version = '0.2.17',
   license='MIT',
   description = 'Ring Attention - Pytorch',
   author = 'Phil Wang',
@@ -18,6 +18,7 @@ setup(
   install_requires=[
     'beartype',
     'einx[torch]>=0.1.3',
+    'einops>=0.7.0',
     'torch>=2.0'
   ],
   classifiers=[
